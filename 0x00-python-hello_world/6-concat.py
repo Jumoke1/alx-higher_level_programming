23352bd488de90162cba5227bd3c176ec3f9dc1e
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-10 01:46:18 +0100

    concat

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = "Welcome to "
-str4 = str3 + str1 + " " + str2 + "!"
-print(str4)
+str1 +=  (" " + str2)
+print(f"Welcome to {str1}!")i
