75ff66b4c6bcdb1d7a9f8414b31bed5897353533
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-16 23:30:49 +0100

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    new_l = my_list[:]
     for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+        if new_l[i] == search:
+            new_l[i] = replace
+    return new_l
