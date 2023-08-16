14e69c411ae9c76dc2a2175fb70fd488b49e0b3d
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-16 21:45:46 +0100

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,12 +3,12 @@
 def square_matrix_simple(matrix=[]):
     rows = len(matrix)
     cols = len(matrix[0])
-`    
+
     squared_m = []
     for i in range(rows):
         squared_row = []
         for j in range(cols):
             squared_row.append(matrix[i][j] ** 2)
-        squared_m.append(squared_row)
+            squared_m.append(squared_row)
 
     return squared_m
