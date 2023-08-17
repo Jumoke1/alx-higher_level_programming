e7a7e59b89f1e7fa5031a4f3212f0048e4c11cd5
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-17 04:04:58 +0100

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,9 @@
 if __name__ == "__main__":
     import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
+    total = 0
 
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+    print("Result: {}".format(total))
