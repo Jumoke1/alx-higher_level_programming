3184278af7f6788e9d5db68e51cb00a9854f759c
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-27 17:21:30 +0100

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-for no in range(0, 10):
-    for dig in range(no + 1, 10):
-        if no == 8 and dig == 9:
-            print("{}{}".format(no, dig))
+for j in range(0, 10):
+    for i in range(1, 10):
+        if j >= i:
+            continue
+        elif j == 8 and i == 9:
+            print("{}{}".format(j, i))
         else:
-            print("{}{}".format(no, dig), end=", ")
+            print("{}{}, ".format(j, i), end="")
