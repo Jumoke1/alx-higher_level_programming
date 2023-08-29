2b92d9955cfe4efa3b1b4cd632d5a70c5656c837
Jumoke1 <kazeemjumoke12@gmail.com>
2023-08-29 18:08:36 +0100

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class Square that defines a square.
+    """
+
+    def __init__(self, length):
+        """
+        Initialize the square with a given length.
+
+        Args:
+            length (float): The length of the sides of the square.
+        """
+        self.length = length
+
+    def define_square(self, size):
+        """
+        Define the size of the square.
+
+        Args:
+            size (float): The size to be set for the square.
+        """
+        self.length = size
