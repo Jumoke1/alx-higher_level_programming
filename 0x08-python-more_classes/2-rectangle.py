4f132c195ebe375260508901de0f82ac0e3e175e
Jumoke1 <kazeemjumoke12@gmail.com>
2023-09-04 23:28:45 +0100

    rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,4 +50,4 @@ class Rectangle:
         if self.__height == 0 or self.width == 0:
             return (0)
 
-        return (2(self.__height + self.__width))
+        return (2 * (self.__height + self.__width))
