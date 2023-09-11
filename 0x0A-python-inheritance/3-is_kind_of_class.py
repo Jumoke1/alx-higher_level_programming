0ab01a9aeaeb6a849f908929e8c78a7af2ab15ff
Jumoke1 <kazeemjumoke12@gmail.com>
2023-09-11 17:40:39 +0100

    instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''A function that retrns true if obj is an instance of an inherited  class.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Check if an onject is an instance or inherited instance.
+
+    Args:
+         obj (any): The object
+         a_class (type): The class to match thhe obj
+    Return:
+         if obj is exactly an instance return true
+         Otherwise - False.
+    '''
+    if isinstance(obl, a_class):
+        return True
+    return False
