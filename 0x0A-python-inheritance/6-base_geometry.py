b3568ff6b2b5220bd6a5bd491b359c3607098b11
Jumoke1 <kazeemjumoke12@gmail.com>
2023-09-12 01:17:17 +0100

    Basegeomentry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define empty class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Represent basegeometry class'''
+
+    def area(self):
+        '''Not iplemented'''
+        raise Exception("area() is not implemented")
