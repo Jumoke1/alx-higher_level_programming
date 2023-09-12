c5db19b4b9f689074a823c6b9f2f35c8a6d4ce9b
Jumoke1 <kazeemjumoke12@gmail.com>
2023-09-12 17:09:44 +0100

    apppend

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines an append function"""
+
+
+def append_write(filename="", text=""):
+    """Append a string at the end of the text file.
+
+     Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters addedd
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)
