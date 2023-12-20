83f103f253fd223911d26feb82b61ee8c473d02e
Jumoke1 <81756146+Jumoke1@users.noreply.github.com>
2023-12-20 21:10:36 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines a python data structure """
-import json
-
-
-def from_json_string(my_str):
-    """Returns an onj represented by json string"""
-    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines an JSON file writting function"""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """write an object to a text file using json"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
