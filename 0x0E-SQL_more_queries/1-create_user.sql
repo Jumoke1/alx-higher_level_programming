bca8cdee251592d4ec596c7f62686e8bb0f073d3
Jumoke1 <kazeemjumoke12@gmail.com>
2024-02-14 13:31:55 -0800

    sql queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
 -- Write a script that creates the MySQL server user user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT CONNECT to user_0d_1 IDENTIFIED BY user_0d_1;
+GRANT CONNECT TO user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGS
 ON *.*
 TO 'user_0d_1'@'localhost';
-
