fef293decbb6c3fa3394c13b3edb29f993afcb75
Jumoke1 <kazeemjumoke12@gmail.com>
2024-02-14 13:37:35 -0800

    sql queies

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT CONNECT TO user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGS
+GRANT ALL PRIVILEGES
 ON *.*
 TO 'user_0d_1'@'localhost';
