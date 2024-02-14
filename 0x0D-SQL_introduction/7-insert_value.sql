e46cec65af6cd1cbfc925848776c95100d8fa8e8
Jumoke1 <kazeemjumoke12@gmail.com>
2024-02-14 05:25:56 -0800

    sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---A script that insert a new row into the table
+-- A script that insert a new row into the table
 INSERT INTO `first_table` (`id` , `name`) VALUES (89 , "Best School");
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- A script that create a table second_table in the database
 CREATE TABLE IF NOT EXIST `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id` `name` `score`) VALUES (1, John, 10);
-INSERT INTO `second_table` (`id` `name` `score`) VALUES (1, Alex, 3);
-INSERT INTO `second_table` (`id` `name` `score`) VALUES (1, Bob, 14);
-INSERT INTO `second_table` (`id` `name` `score`) VALUES (1, George, 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, John, 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, Alex, 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, Bob, 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, George, 8);
