8b693475fa128de67e233a6a031acfe5e2a3c2c7
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-13 23:05:16 +0100

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const firstLine = 'C is fun'; 
-const  secondLine = 'Python is cool';
-const thirdLine =  'JavaScript is amazing';
-console.log(firstList, SecondLine, thirdLine);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
