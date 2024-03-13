46cd063e74df526be857beae3e6695253b8569bf
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-13 17:54:24 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement)
+exports.nbOccurences = function (list, searchElement) {
 return list.reduce ((accumulator, currentValue) => currentValue === searchElement ? accumulator + 1 : accumulator, 0);
+};
