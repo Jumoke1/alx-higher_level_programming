68be423471c8992c3d9e86e8e4b46da889112bd9
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-28 16:40:01 -0700

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if__name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html_content  = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html_content)))
+    print("\t- content: {}".format(html_content))
+    print("\t- utf8 content: {}".format(html_content.decode("utf-8")))
