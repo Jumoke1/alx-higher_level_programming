199e35238a9f8331ffb8f638f22e5c3a756f644b
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-28 17:33:09 -0700

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html_content  = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    as response:
+        html_content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html_content)))
         print("\t- content: {}".format(html_content))
