3d98c708bee3a671a60e8050bb4c95d0e7331261
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-29 18:10:29 -0700

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""Write a script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
@@ -9,7 +10,8 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'"
+                "ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
